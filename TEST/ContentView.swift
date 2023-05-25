f62e0f105c4cfd904a09f002e574399c69aedb58
ZaidCho <zaid.cho@nufyn.com>
2023-05-25 10:45:03 +0900

    Initial Commit

---
+++ TEST/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TEST
+//
+//  Created by Zaid.Cho on 2023/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Zaid.Cho on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
